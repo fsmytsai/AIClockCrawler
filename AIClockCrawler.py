cbc73e9b11c1523025760447d692b385842641c1
fsmytsai <msphausung1998@gmail.com>
2018-02-17 04:55:14 +0800

    Update

--- AIClockCrawler.py
+++ AIClockCrawler.py
@@ -25,7 +25,7 @@ class AIClockCrawler:
         self.category = category
         oldmask = os.umask(000)
         os.makedirs(self.log_absolute_path + '%s' %
-                    (datetime.now().strftime('%Y-%m-%d')), 0o755, True)
+                    (datetime.now().strftime('%Y-%m-%d')), 0o777, True)
         os.umask(oldmask)
         self.logFile = open(self.log_absolute_path + '%s.txt' %
                             (datetime.now().strftime('%Y-%m-%d/%H-%M-%S')), 'w', encoding='utf-8')
